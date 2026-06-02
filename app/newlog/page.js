5c15bd80779de8f8926cb79098321279f8b4b56d
Ayush <ayushsareen793@gmail.com>
2026-06-03 00:57:21 +0530

    new log form

--- app/newlog/page.js
+++ app/newlog/page.js
@@ -29,26 +29,26 @@ const page = () => {
 
           <div className="mb-5">
             <label className="block text-sm font-medium text-[#1c1c19] mb-1.5">Log title</label>
-            <input type="text" placeholder="eg. Quiet Temples of Kalpa" className="w-full bg-[#f7f5f0] px-4 py-2.5  border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
+            <input type="text" placeholder="eg. Quiet Temples of Kalpa" className="w-full bg-white px-4 py-2.5  border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
           </div>
 
           {/* country and city/region */}
           <div className="grid grid-cols-2 gap-4 mb-5">
             <div>
               <label className="block text-sm font-medium text-[#1c1c19] mb-1.5">Country</label>
-              <input type="text" placeholder="e.g. India" className="w-full px-4 py-2.5 bg-[#f7f5f0] border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
+              <input type="text" placeholder="e.g. India" className="w-full px-4 py-2.5 bg-white border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
             </div>
 
             <div>
               <label className="block text-sm font-medium text-[#1c1c19] mb-1.5">City / Region</label>
-              <input type="text" placeholder="e.g. Shimla" className="w-full px-4 py-2.5 bg-[#f7f5f0] border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
+              <input type="text" placeholder="e.g. Shimla" className="w-full px-4 py-2.5 bg-white border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
             </div>
           </div>
 
           {/* date */}
           <div className="mb-5">
             <label className="block text-sm font-medium text-[#1c1c19] mb-1.5">Date Of Visit</label>
-            <input type="date" placeholder="dd-mm-yyyy" className="w-full px-4 py-2.5 bg-[#f7f5f0] border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
+            <input type="date" placeholder="dd-mm-yyyy" className="w-full px-4 py-2.5 bg-white border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
           </div>
 
 
@@ -110,19 +110,82 @@ const page = () => {
           {/* cover photo */}
           <p className="text-l font-bold uppercase tracking-[3px] text-[#2D4B37] mb-4">Cover Photo</p>
           <div className="border-2 border-dashed border-[#e8e4da] rounded-2xl p-10 text-center bg-white hover:border-[#2D4B37] hover:bg-[#f4f9f6] transition-all duration-150 cursor-pointer mb-8">
-            <img src="https://img.icons8.com/color/96/image.png" className="w-12 h-12 mx-auto mb-3"/>
+            <img src="https://img.icons8.com/color/96/image.png" className="w-12 h-12 mx-auto mb-3" />
             <p className="text-sm font-medium text-[#1c1c19] mb-1">Click to upload cover photo</p>
-            <p className="text-xs text-secondary">PNG, JPG or WEBP · Max 5MB</p>
-            <input type="file" accept="image/*" className="hidden"/>
+            <p className="text-xs text-secondary  text-gray-400">PNG, JPG or WEBP · Max 5MB</p>
+            <input type="file" accept="image/*" className="hidden" />
+          </div>
+
+
+          {/* divider */}
+          <div className='h-px bg-[#e8e4da] my-8'></div>
+
+
+
+          {/* your log */}
+          <p className="text-lg font-bold uppercase tracking-[3px] text-[#2D4B37] mb-4">Your Log</p>
+
+          <label className="block text-sm font-medium text-[#1c1c19] mb-1.5">About Place</label>
+          <div className="mb-5">
+            <textarea placeholder="Describe the place: what made it special, what it felt like, memorable moments, local culture, food, or anything that stood out." maxLength={2000} rows={6} className="border-2  border-[#e8e4da] rounded-2xl px-4 py-3 w-full bg-white resize-none focus:outline-none focus:border-[#2D4B37] text-gray-700 placeholder:text-gray-400" />
+          </div>
+
+          <label className="block text-sm font-medium text-[#1c1c19] mb-1.5">Best Time To Visit</label>
+          <div>
+            <input type="text" placeholder="eg. OffSeason or October/september" className="w-full bg-white px-4 py-2.5  border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
+          </div>
+
+          <label className="block text-sm font-medium text-[#1c1c19] mb-1.5 pt-5">How To Get There</label>
+          <div>
+            <textarea placeholder="e.g. Transportation Options, Best Route To Get There" rows={4} className="w-full bg-white px-4 py-3 border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150 resize-none" />
+          </div>
+
+
+          {/* divider */}
+          <div className='h-px bg-[#e8e4da] my-8'></div>
+
+
+          {/* hidden gems */}
+          <p className="text-lg font-bold uppercase tracking-[3px] text-[#2D4B37] mb-4">Hidden Gems</p>
+          <label className='block text-sm font-medium mb-1.5 text-gray-800 '>Add The Underrated Spots Only You Know About.</label>
+          <div className='pt-3 w-3xl'>
+            <input type="text" placeholder="eg. Food Spots" className="w-full bg-white  px-4 py-2.5  border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
+          </div>
+          <div className='pt-2 w-3xl'>
+            <input type="text" placeholder="eg. Secret Waterfall Between The Trek" className="w-full bg-white  px-4 py-2.5  border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150" />
+          </div>
+
+
+          {/* divider */}
+          <div className='h-px bg-[#e8e4da] my-8'></div>
+
+
+
+
+          {/* tips and recommendation */}
+          <p className="text-lg font-bold uppercase tracking-[3px] text-[#2D4B37] mb-4">Tips & Recommendations</p>
+
+          <label className="block text-sm font-medium text-[#1c1c19] mb-1.5 pt-5">Where To Eat</label>
+          <div>
+            <textarea placeholder="e.g. Your Favourite Spots,Local Dishes. " rows={4} className="w-full bg-white px-4 py-3 border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150 resize-none" />
+          </div>
+
+          <label className="block text-sm font-medium text-[#1c1c19] mb-1.5 pt-5">Where To Stay</label>
+          <div>
+            <textarea placeholder="e.g. Budget Hotels,Areas To Stay" rows={4} className="w-full bg-white px-4 py-3 border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150 resize-none" />
+          </div>
+
+          <label className="block text-sm font-medium text-[#1c1c19] mb-1.5 pt-5">Things To Avoid</label>
+          <div>
+            <textarea placeholder="e.g. Tourist Traps,What Not To Do.." rows={4} className="w-full bg-white px-4 py-3 border border-[#e8e4da] rounded-xl text-sm text-[#1c1c19] placeholder:text-gray-500 focus:outline-none focus:ring-4 focus:ring-[#2D4B37]/10 focus:border-[#2D4B37] transition-all duration-150 resize-none" />
           </div>
 
 
 
-
-
-
-
-
+          {/* save button */}
+          <div className='pt-5 mx-auto flex justify-center'>
+            <button type="submit" className="bg-transparent mx-auto w-5xl text-[#2D4B37] border border-[#2D4B37] hover:bg-[#2D4B37] hover:text-white focus:ring-4 focus:ring-[#2D4B37]/20 focus:outline-none shadow-xs font-medium leading-5 rounded-xl text-sm px-4 py-2.5 transition-colors duration-150">Publish Log</button>
+          </div>
 
 
 
